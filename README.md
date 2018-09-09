c5fab9ad9469d8ec5293aa041632961141d8c22a
SirSerje <sir.serje@gmail.com>
2018-09-09 11:44:45 +0200

    add comments, update readme, locate todos

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # hello-node
 simple node express app to discover basic api building
 
+### api methods:
+
+`/items/?page=3&size=20` get page of elements
+
+**page** (_required_) - page number
+
+**size** (_required_) - items per one page to be returned
+
+
+`/item/?article=XXXXXX` get article details :no_entry:
+
+**article** (_required_) - id of article
+
+
 ### avaliable scripts:
 
 `start` : based on nodemon, just simply start this server
--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
-const fs = require('fs');
-const url = require('url');
-const express = require('express');
-const queryString = require('querystring');
-const app = express();
-const PORT = 3567;
+//define all dependencies
+const fs = require('fs'); //FileSystem - any work with physically saved files
+const url = require('url'); //forms request object based on url you,re enter in search bar
+const express = require('express'); //module to simplify work with node
+const queryString = require('querystring'); //module, which separate query into object's key-value
+const app = express(); //start express
+const PORT = 3567; //default port, just to save time
 
-//define global store variable
-let globalData;
-init();
+let globalData; //define global store variable
+init(); //add data to global store
 
-app.get('/api/*', (req, res) => {
+
+app.get('/items/*', (req, res) => {
+  //TODO: https://github.com/SirSerje/hello-node/issues/2
   let temp = globalData;
   let url_parts = url.parse(req.url);
   let requestSettings = queryString.parse(url_parts.query);
 
   let pages = getNewsPerPage(requestSettings);
 
-
-if(pages === undefined) res.sendStatus(404);
+  if (pages === undefined) res.sendStatus(404);
   res.send(temp.slice(pages.from, pages.to))
   // res.sendStatus(200)
+});
+
+app.get('/item/*', (req, res) => {
+  //TODO: implementation needed
 });
 
 app.listen(PORT, () => console.log(`example on ${PORT}`));
@@ -28,7 +33,9 @@ function init() {
   globalData = JSON.parse(fs.readFileSync('./public/d.json', 'utf8'));
 }
 
+/* method to validate is correct page, human requested*/
 function getNewsPerPage(props) {
+  //FIXME: https://github.com/SirSerje/hello-node/issues/1
   let {size, page} = props;
   size = Number(size);
   page = Number(page);
@@ -36,7 +43,7 @@ function getNewsPerPage(props) {
   let to = size * page;
   let from = to - size;
 
-  if(to > length) return undefined;
+  if (to > length) return undefined;
   return {from, to}
 
 }
