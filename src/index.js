fbe53aa4224e4eb4c12ae01ff7f084046ec48807
SirSerje <sir.serje@gmail.com>
2018-09-09 10:56:40 +0200

    correct pagination implemented. add gitignore

--- src/index.js
+++ src/index.js
@@ -1,39 +1,44 @@
-const fs = require('fs');
-const url = require('url');
+const fs = require('fs')
+const url = require('url')
 const express = require('express')
-const queryString = require('querystring');
+const queryString = require('querystring')
 const app = express()
 const PORT = 3567
 
+//define global store variable
 let globalData
 init()
 
-app.get('/', (req, res) => {
-  let temp = globalData
-  console.log(globalData.length)
-  res.send(temp)
-  // res.sendStatus(200)
-})
-
-//TODO: need support query request
-// queryString.parse('size=1&page=2');
-//https://www.w3schools.com/nodejs/ref_querystring.asp
 app.get('/api/*', (req, res) => {
   let temp = globalData
-  console.log(globalData.length)
+  let url_parts = url.parse(req.url)
+  let requestSettings = queryString.parse(url_parts.query)
+
+  getNewsPerPage(requestSettings)
   res.send(temp)
   // res.sendStatus(200)
 })
 
 app.listen(PORT, () => console.log(`example on ${PORT}`))
 
-//run on server start
 function init() {
-
   globalData = JSON.parse(fs.readFileSync('./public/d.json', 'utf8'));
-  /*  fs.readFile('./public/d.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-    globalData = data;
-  });*/
+}
+
+function getNewsPerPage(props) {
+  let {size, page} = props
+  size = Number(size)
+  page = Number(page)
+  let length = globalData.length
+  let to = size * page - 1
+  let from = to - size + 1
+
+  for (let i = from; i <= to; i++) {
+    if (i > length) {
+      console.log('OVER')
+      break
+    } else {
+      console.log('element:', i)
+    }
+  }
 }
